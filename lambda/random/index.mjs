e87c692ccc82b887fedb3cee38bdac6241f03bec
Aiden Payne <aiden@thepaynes.com.au>
2023-05-25 16:00:57 +1000

    general fixes and improvements following updated dynamodb work.

--- lambda/random/index.mjs
+++ lambda/random/index.mjs
@@ -1,11 +1,47 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const dynamoDBTableName = process.env.DYNAMO_TABLE_NAME;
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+
 export const handler = async (event) => {
-  // TODO implement random dad joke logic to retrieve from DynamoDB.
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      id: "asdf1234",
-      joke: "Why did the scarecrow win an award? Because he was outstanding in his field.",
-    }),
+  let body;
+  let statusCode = 200;
+  let headers = {
+    "Content-Type": "application/json",
   };
-  return response;
+
+  // TODO perform a count operation on the table to determine the maximum number to use for random generation dynamically.
+  const dynamoDBJokeCount = 10;
+
+  const randInt = generateRandomIntBelowMax(dynamoDBJokeCount);
+
+  try {
+    body = await dynamo.send(
+      new GetCommand({
+        TableName: dynamoDBTableName,
+        Key: {
+          Id: randInt,
+        },
+      })
+    );
+  } catch (err) {
+    // Note: This is not returning the err.message as this could be a potential security flaw.
+    statusCode = 500;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
+
+// TODO move this into an application layer as it might be useful for other lambdas down the track.
+const generateRandomIntBelowMax = (max, min = 1) => {
+  return Math.round(Math.random() * (max - min) + min);
 };
